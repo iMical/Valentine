8288480ce23a260f5fefc42aa6f1d794259bc555
Mical <mical.idica@gmail.com>
2025-02-12 18:51:02 -1000

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('nobutton');
-
-button.addEventListener('click', function(){
-	if (button.textContent === 'no') {
-		button.textContent = 'are you really sure?';
-	} else {
-		button.textContent = 'no';
-	}
-});
